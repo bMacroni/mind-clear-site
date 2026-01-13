20e37ba9688023a9376d4c1275f8b496384b2186
bMacroni <bmcornell88@gmail.com>
2026-01-12 21:30:37 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import InteractiveHero from "../components/InteractiveHero";
 import Mission from "../components/Mission";
 import FeaturedProject from "../components/FeaturedProject";
@@ -8,17 +8,9 @@ import ScrollTelling from "../components/ScrollTelling";
 import Footer from "../components/Footer";
 
 export default function Home() {
-  const [scrollY, setScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => setScrollY(window.scrollY);
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="bg-[#121212] min-h-screen">
-      <InteractiveHero scrollY={scrollY} />
+      <InteractiveHero />
       <Mission />
       <FeaturedProject />
       <ScrollTelling />
